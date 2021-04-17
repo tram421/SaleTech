82327d3232e3a5e7b2b10d2bcb52c3652de5f571
tram421 <maitram0291@gmail.com>
2021-04-18 06:27:09 +0700

    giao dien search trong homeFragment

--- app/src/main/java/com/tram/saletech/Fragment/HomeFragment.java
+++ app/src/main/java/com/tram/saletech/Fragment/HomeFragment.java
@@ -62,6 +62,8 @@ public class HomeFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_home, container, false);
+
+
         return view;
     }
 }
